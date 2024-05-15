56eb2d45b096b4688b83ff5ab898ff17b7cdff0a
Javier Rivasseau <javierspawn@gmail.com>
2024-05-15 14:04:49 -0300

    Corrected ports

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const cors = require("cors");
-const blogRouter = require('./route/index')
-
-require('./db')
+const blogRouter = require('./route/index');
+require('./db');
 
 const app = express();
+const PORT = process.env.PORT || 5000; // Usar variable de entorno o puerto predeterminado
+
 app.use(cors());
 app.use(express.json());
-
-app.use('/api/blogs', blogRouter)
+app.use('/api/blogs', blogRouter);
 
 app.use("/api", (req, res) => {
   res.send("Hello World");
 });
 
-app.listen(5000, () => console.log("App is running at 5000"));
+app.listen(PORT, () => console.log(`App is running at ${PORT}`));
